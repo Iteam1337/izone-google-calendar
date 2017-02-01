7b86d92c3f1ff9ffe2404b9de9fe8f8b1846683a
ilix <src@ilix.se>
2017-02-01 20:57:49 +0100

    Refactored getDatesForWeek() so that it returns an object.

--- test/unit/helpers/weekHelper.js
+++ test/unit/helpers/weekHelper.js
@@ -6,7 +6,7 @@ const expect = chai.expect
 const helper = require(process.cwd() + '/lib/helpers/weekHelper')
 
 describe('weekHelper', () => {
-  describe('addWeek', () => {
+  describe('addWeek()', () => {
     it('2017w01 + 1', () => {
       expect(helper.addWeek('2017w01', 1)).to.eql('2017w02')
     })
@@ -20,28 +20,28 @@ describe('weekHelper', () => {
       expect(helper.addWeek('2017w02', -10)).to.eql('2016w44')
     })
   })
-  describe('getDatesForWeek()', () => {
+  describe('getWeek()', () => {
     it('201705', () => {
-      expect(helper.getDatesForWeek('2017w05')).to.eql([
-        '2017-01-30',
-        '2017-01-31',
-        '2017-02-01',
-        '2017-02-02',
-        '2017-02-03',
-        '2017-02-04',
-        '2017-02-05'
-      ])
+      expect(helper.getWeek('2017w05')).to.eql({
+        Monday: '2017-01-30',
+        Tuesday: '2017-01-31',
+        Wednesday: '2017-02-01',
+        Thursday: '2017-02-02',
+        Friday: '2017-02-03',
+        Saturday: '2017-02-04',
+        Sunday: '2017-02-05'
+      })
     })
     it('201553', () => {
-      expect(helper.getDatesForWeek('2015w53')).to.eql([
-        '2015-12-28',
-        '2015-12-29',
-        '2015-12-30',
-        '2015-12-31',
-        '2016-01-01',
-        '2016-01-02',
-        '2016-01-03'
-      ])
+      expect(helper.getWeek('2015w53')).to.eql({
+        Monday: '2015-12-28',
+        Tuesday: '2015-12-29',
+        Wednesday: '2015-12-30',
+        Thursday: '2015-12-31',
+        Friday: '2016-01-01',
+        Saturday: '2016-01-02',
+        Sunday: '2016-01-03'
+      })
     })
   })
 
