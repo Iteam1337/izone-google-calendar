73a4a4603cdc815b2d8f21e63aa156f3ab5b0b0c
ilix <src@ilix.se>
2017-02-11 10:29:15 +0100

    Successfully ignore events with empty descriptions.

--- test/unit/helpers/aliasHelper.spec.js
+++ test/unit/helpers/aliasHelper.spec.js
@@ -7,14 +7,31 @@ const helper = require(process.cwd() + '/lib/helpers/aliasHelper')
 
 describe('aliasHelper', () => {
   describe('isWorkout()', () => {
-    it('träna: crossfit', () => {
-      expect(helper.isWorkout('träna: crossfit')).to.eql(true)
+    describe('can parse', () => {
+      it('träna: crossfit', () => {
+        expect(helper.isWorkout('träna: crossfit')).to.eql(true)
+      })
+      it('TRÄNA: Lunchlöpning', () => {
+        expect(helper.isWorkout('TRÄNA: Lunchlöpning')).to.eql(true)
+      })
+      it('Dagens pass helg', () => {
+        expect(helper.isWorkout('Dagens pass helg')).to.eql(true)
+      })
     })
-    it('TRÄNA: Lunchlöpning', () => {
-      expect(helper.isWorkout('TRÄNA: Lunchlöpning')).to.eql(true)
-    })
-    it('Dagens pass helg', () => {
-      expect(helper.isWorkout('Dagens pass helg')).to.eql(true)
+
+    describe('can handle unexpected events', () => {
+      it('.s,d.s,d.sd', () => {
+        expect(helper.isWorkout('.s,d.s,d.sd')).to.eql(false)
+      })
+      it('(empty string)', () => {
+        expect(helper.isWorkout('')).to.eql(false)
+      })
+      it('(null)', () => {
+        expect(helper.isWorkout(null)).to.eql(false)
+      })
+      it('(undefined)', () => {
+        expect(helper.isWorkout(undefined)).to.eql(false)
+      })
     })
   })
 })
