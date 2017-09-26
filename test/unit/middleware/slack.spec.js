3d7c6c1601091541f8988f6a6229788d9e9f1faa
ilix <src@ilix.se>
2017-09-26 11:11:40 +0200

    Wrote more tests for the slack middleware.

--- test/unit/middleware/slack.spec.js
+++ test/unit/middleware/slack.spec.js
@@ -19,6 +19,7 @@ describe('slack middleware', () => {
     }
 
     res = {
+      end: stub(),
       send: stub()
     }
 
@@ -34,6 +35,9 @@ describe('slack middleware', () => {
   })
 
   describe('user', () => {
+    const userId = 'meow123'
+    const userName = 'Longcat'
+
     beforeEach(() => {
       req.params = {
         payload: JSON.stringify({})
@@ -41,6 +45,7 @@ describe('slack middleware', () => {
     })
 
     it('calls databaseAdapter', () => {
+      req.params.user_id = userId
       return middleware.user(req, res, next)
         .then(() => {
           expect(databaseAdapter.getPersonBySlackIdentity).callCount(1)
@@ -48,9 +53,6 @@ describe('slack middleware', () => {
     })
 
     it('passes slack id and slack name for izone user mapping', () => {
-      const userId = 'meow123'
-      const userName = 'Longcat'
-
       req.params.user_id = userId
       req.params.user_name = userName
 
@@ -60,6 +62,51 @@ describe('slack middleware', () => {
             userId,
             userName
           })
+        })
+    })
+
+    it('sets req.izoneUser, when person can be mapped', () => {
+      req.params.user_id = userId
+      req.params.user_name = userName
+
+      const izoneUser = {
+        p_id: 1,
+        p_slack_user_id: userId,
+        p_slack_user_name: userName
+      }
+      databaseAdapter.getPersonBySlackIdentity = stub().resolves(izoneUser)
+
+      return middleware.user(req, res, next)
+        .then(() => {
+          expect(next).callCount(1)
+          expect(req.izoneUser.p_id).to.eql(1)
+          expect(req.izoneUser.p_slack_user_id).to.eql(userId)
+          expect(req.izoneUser.p_slack_user_name).to.eql(userName)
+        })
+    })
+
+    it('returns error 401, when person cannot be mapped', () => {
+      req.params.user_id = userId
+      req.params.user_name = userName
+
+      databaseAdapter.getPersonBySlackIdentity = stub().resolves({})
+
+      return middleware.user(req, res, next)
+        .then(() => { expect('should go to catch()').to.eql('') })
+        .catch(() => {
+          expect(next).callCount(1)
+          expect(res.statusCode).to.eql(401)
+          expect(req.izoneUser).to.eql(undefined)
+        })
+    })
+
+    it('returns error 500, if no user is set on req.params', () => {
+      return middleware.user(req, res, next)
+        .then(() => { expect('should go to catch()').to.eql('') })
+        .catch(() => {
+          expect(next).callCount(1)
+          expect(res.statusCode).to.eql(500)
+          expect(req.izoneUser).to.eql(undefined)
         })
     })
   })
