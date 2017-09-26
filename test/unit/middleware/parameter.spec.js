3d647ed47c4c5b8092fa72e957957c754193ab41
ilix <src@ilix.se>
2017-09-26 16:39:34 +0200

    Created a new middleware for parsing parameters passed in the text field from slack.

---
+++ test/unit/middleware/parameter.spec.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const chai = require('chai')
+const expect = chai.expect
+const {stub} = require('sinon')
+const proxyquire = require('proxyquire')
+
+require('sinon-as-promised')
+chai.use(require('sinon-chai'))
+
+describe('slack middleware', () => {
+  let middleware
+  let req, res, next
+
+  beforeEach(() => {
+    req = {
+      params: {}
+    }
+
+    res = {
+      end: stub(),
+      send: stub()
+    }
+
+    next = stub()
+
+    middleware = proxyquire(process.cwd() + '/lib/middleware/parameter', {
+    })
+  })
+
+  describe('parameter', () => {
+    it('can parse week', () => {
+      req.params.text = '/time week=2017w12'
+      middleware.parseParameters(req, res, next)
+      expect(req.izone.week).to.eql('2017w12')
+    })
+  })
+})
