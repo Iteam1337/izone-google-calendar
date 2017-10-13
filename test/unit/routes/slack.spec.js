b08321133b005bdd64e5de564aa510940b2843d2
ilix <src@ilix.se>
2017-10-13 14:19:56 +0200

    Wrote more unit tests for the slack import route method.

--- test/unit/routes/slack.spec.js
+++ test/unit/routes/slack.spec.js
@@ -9,7 +9,7 @@ require('sinon-as-promised')
 chai.use(require('sinon-chai'))
 
 describe('slack route', () => {
-  let route
+  let sut
   let req, res, next
   let databaseAdapter
   let izoneService, request, slackService, googleAdapter
@@ -44,7 +44,7 @@ describe('slack route', () => {
       markEventImported: stub().resolves()
     }
 
-    route = proxyquire(process.cwd() + '/lib/routes/slack', {
+    sut = proxyquire(process.cwd() + '/lib/routes/slack', {
       '../adapters/database': databaseAdapter,
       '../services/izone': izoneService,
       'request': request,
@@ -65,7 +65,7 @@ describe('slack route', () => {
     })
 
     it('calls slackService', () => {
-      return route.summary(req, res, next)
+      return sut.summary(req, res, next)
         .then(() => {
           expect(slackService.summary).callCount(1)
         })
@@ -75,7 +75,7 @@ describe('slack route', () => {
       req.params = {
         payload: JSON.stringify({ response_url: 'http://izone.test' })
       }
-      return route.import(req, res, next)
+      return sut.import(req, res, next)
         .then(() => {
           expect(izoneService.getAllEvents).callCount(1)
         })
@@ -134,7 +134,7 @@ describe('slack route', () => {
     })
 
     it('respects user\'s autoimport setting and does not import anything if import parameter is not set', () => {
-      return route.import(req, res, next)
+      return sut.import(req, res, next)
         .then(() => {
           console.log()
           expect(databaseAdapter.import).callCount(0)
@@ -144,7 +144,7 @@ describe('slack route', () => {
     it('respects user\'s autoimport setting and skips alias not defined in import parameter', () => {
       req.izone.import = 'somethingelse'
 
-      return route.import(req, res, next)
+      return sut.import(req, res, next)
         .then(() => {
           console.log()
           expect(databaseAdapter.import).callCount(0)
@@ -179,7 +179,7 @@ describe('slack route', () => {
       }
       izoneService.getAllEvents = stub().resolves(events)
 
-      return route.import(req, res, next)
+      return sut.import(req, res, next)
         .then(() => {
           expect(databaseAdapter.import).callCount(1)
         })
@@ -213,7 +213,7 @@ describe('slack route', () => {
       }
       izoneService.getAllEvents = stub().resolves(events)
 
-      return route.import(req, res, next)
+      return sut.import(req, res, next)
         .then(() => {
           console.log()
           expect(databaseAdapter.import).callCount(2)
@@ -224,36 +224,215 @@ describe('slack route', () => {
   /*
    * Import logic tests with regard to alias job_db mapping.
    */
-  describe('import() handles aliases correctly', () => {
+  describe('import() handles aliases correctly when importing new hours', () => {
+    const aliases = {
+      hasJob: 'izone',
+      doesNotHaveJob: 'izoneblablabla',
+      havingMoreThanOneJob: 'overusedalias'
+    }
+
     beforeEach(() => {
       req.params = {
         payload: JSON.stringify({})
       }
 
-      izoneService.getAllEvents = stub().resolves({
-        calendar: []
-      })
+      req.izone = {
+        user: {
+          p_izone_autoimport: true,
+          p_izusername: 'abc'
+        },
+        google: {
+          accessToken: 'zaq12wsxcde34rfv'
+        }
+      }
+
+      databaseAdapter.getJobByAlias
+        .withArgs(aliases.doesNotHaveJob)
+        .resolves([])
+
+      databaseAdapter.getJobByAlias
+        .withArgs(aliases.hasJob)
+        .resolves([
+          {
+            job_alias: aliases.hasJob
+          }
+        ])
+
+      databaseAdapter.getJobByAlias
+        .withArgs(aliases.havingMoreThanOneJob)
+        .resolves([
+          {
+            job_alias: aliases.havingMoreThanOneJob
+          },
+          {
+            job_alias: aliases.havingMoreThanOneJob
+          }
+        ])
     })
 
     it('does not import hours if alias cannot be found in database', () => {
-      expect(true).to.eql(false)
+      izoneService.getAllEvents = stub().resolves({
+        calendar: [
+          {
+            end: {
+              dateTime: moment()
+            },
+            start: {
+              dateTime: moment()
+            },
+            summary: `${aliases.doesNotHaveJob}: test`
+          }
+        ],
+        izone: [] // Nothing imported yet.
+      })
+
+      return sut.import(req, res, next)
+        .then(() => {
+          expect(databaseAdapter.import)
+            .callCount(0)
+        })
     })
 
     it('does not import hours if alias maps to more than one project', () => {
-      expect(true).to.eql(false)
+      izoneService.getAllEvents = stub().resolves({
+        calendar: [
+          {
+            end: {
+              dateTime: moment()
+            },
+            start: {
+              dateTime: moment()
+            },
+            summary: `${aliases.havingMoreThanOneJob}: test`
+          }
+        ],
+        izone: [] // Nothing imported yet.
+      })
+
+      return sut.import(req, res, next)
+        .then(() => {
+          expect(databaseAdapter.import)
+            .callCount(0)
+        })
     })
 
     it('does not import hours if time entry has not ended yet', () => {
-      expect(true).to.eql(false)
+      izoneService.getAllEvents = stub().resolves({
+        calendar: [
+          {
+            end: {
+              dateTime: moment().add(1, 'hours') // Ends in the future - not going to be imported.
+            },
+            start: {
+              dateTime: moment().add(-1, 'hours')
+            },
+            summary: `${aliases.hasJob}: doing some work right now`
+          },
+          {
+            end: {
+              dateTime: moment().add(-1, 'hours')
+            },
+            start: {
+              dateTime: moment().add(-2, 'hours')
+            },
+            summary: `${aliases.hasJob}: did some work an hour ago`
+          }
+        ],
+        izone: [] // Nothing imported yet.
+      })
+
+      return sut.import(req, res, next)
+        .then(() => {
+          expect(databaseAdapter.import)
+            .callCount(1)
+        })
     })
 
     it('does not import hours that contain no alias and are not workouts', () => {
-      // TODO: Write a test with a time entry that LACKS : in the name.
-      expect(true).to.eql(false)
+      izoneService.getAllEvents = stub().resolves({
+        calendar: [
+          {
+            end: {
+              dateTime: moment()
+            },
+            start: {
+              dateTime: moment()
+            },
+            summary: 'work work work' // Not an actual alias.
+          }
+        ],
+        izone: [] // Nothing imported yet.
+      })
+
+      return sut.import(req, res, next)
+        .then(() => {
+          expect(databaseAdapter.import)
+            .callCount(0)
+        })
     })
 
-    it('imports hours if alias maps to a single "job" in the database', () => {
-      expect(true).to.eql(false)
+    it('imports a time entry if alias maps to a single "job" in the database', () => {
+      izoneService.getAllEvents = stub().resolves({
+        calendar: [
+          {
+            end: {
+              dateTime: moment()
+            },
+            start: {
+              dateTime: moment()
+            },
+            summary: `${aliases.hasJob}: test`
+          }
+        ],
+        izone: [] // Nothing imported yet.
+      })
+
+      return sut.import(req, res, next)
+        .then(() => {
+          expect(databaseAdapter.import)
+            .callCount(1)
+        })
+    })
+
+    it('imports time entries if alias maps to a single "job" in the database', () => {
+      izoneService.getAllEvents = stub().resolves({
+        calendar: [
+          {
+            end: {
+              dateTime: moment()
+            },
+            start: {
+              dateTime: moment()
+            },
+            summary: `${aliases.hasJob}: work work work`
+          },
+          {
+            end: {
+              dateTime: moment()
+            },
+            start: {
+              dateTime: moment()
+            },
+            summary: `${aliases.hasJob}: test`
+          },
+          {
+            end: {
+              dateTime: moment()
+            },
+            start: {
+              dateTime: moment()
+            },
+            summary: `${aliases.hasJob}: test`
+          }
+        ],
+        izone: [] // Nothing imported yet.
+      })
+
+      return sut.import(req, res, next)
+        .then(() => {
+          expect(databaseAdapter.import)
+            .callCount(3)
+        })
     })
   })
 })
