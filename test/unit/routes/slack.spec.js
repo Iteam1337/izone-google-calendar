ef0dcc8a2d11699243229d396279d66c412d308f
ilix <src@ilix.se>
2017-10-13 15:31:17 +0200

    Wrote a couple more tests for updating time entries.

--- test/unit/routes/slack.spec.js
+++ test/unit/routes/slack.spec.js
@@ -505,5 +505,91 @@ describe('routes/slack', () => {
             })
         })
     })
+
+    it('updates event if duration differs', () => {
+      const startTime = moment('2017-06-30 09:00:00')
+
+      const endTimeCalendar = moment('2017-06-30 10:00:00')
+      const endTimeIzone = moment('2017-06-30 11:00:00')
+
+      izoneService.getAllEvents = stub().resolves({
+        calendar: [
+          {
+            end: {
+              dateTime: endTimeCalendar
+            },
+            start: {
+              dateTime: startTime
+            },
+            summary: `${alias}: working`,
+            id: '1q2w3e'
+          }
+        ],
+        izone: [
+          {
+            jl_endtime: endTimeIzone,
+            jl_starttime: startTime,
+            jl_description: 'working',
+            jl_hours: 2,
+            jl_alias: alias,
+            jl_gcal_id: '1q2w3e'
+          }
+        ]
+      })
+
+      return sut.import(req, res, next)
+        .then(() => {
+          expect(databaseAdapter.update)
+            .callCount(1)
+            .calledWith('1q2w3e', {
+              jl_starttime: startTime.format('YYYY-MM-DD HH:mm:ss'),
+              jl_endtime: endTimeCalendar.format('YYYY-MM-DD HH:mm:ss'),
+              jl_hours: 1,
+              jl_description: 'working'
+            })
+        })
+    })
+
+    it('updates event if description differs', () => {
+      const startTime = moment('2017-06-30 09:00:00')
+      const endTime = moment('2017-06-30 10:00:00')
+
+      izoneService.getAllEvents = stub().resolves({
+        calendar: [
+          {
+            end: {
+              dateTime: endTime
+            },
+            start: {
+              dateTime: startTime
+            },
+            summary: `${alias}: doing stuff and doing some work`,
+            id: '1q2w3e'
+          }
+        ],
+        izone: [
+          {
+            jl_endtime: endTime,
+            jl_starttime: startTime,
+            jl_description: 'doing stuff',
+            jl_hours: 1,
+            jl_alias: alias,
+            jl_gcal_id: '1q2w3e'
+          }
+        ]
+      })
+
+      return sut.import(req, res, next)
+        .then(() => {
+          expect(databaseAdapter.update)
+            .callCount(1)
+            .calledWith('1q2w3e', {
+              jl_starttime: startTime.format('YYYY-MM-DD HH:mm:ss'),
+              jl_endtime: endTime.format('YYYY-MM-DD HH:mm:ss'),
+              jl_hours: 1,
+              jl_description: 'doing stuff and doing some work'
+            })
+        })
+    })
   })
 })
